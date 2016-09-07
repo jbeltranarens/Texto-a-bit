66c0477218f06ebf4e7e08b08ed44044dc1ce6b1
jbeltranarens <jose.beltran.a@usach.cl>
2016-09-07 01:17:22 -0300

    Create main.py
    Codigo que convierte la wea
    ocupe raw_input porque con string me daba problemas (string=palabras no numeros) en cambio input funciona solo numeros creo

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#Codigo que convierte la wea
+#ocupe raw_iput porque con string me daba problemas (string=palabras no numeros) en cambio input funciona solo numeros creo
+
+st = raw_input("ingrese algun text :")
+
+print (' '.join(format(ord(x), 'b') for x in st))
