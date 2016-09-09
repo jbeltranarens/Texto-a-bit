e48f10a2d6c25809d552c5a49a154c72bb290f9f
lenin27 <lenin.romero@usach.cl>
2016-09-09 02:51:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,6 @@ st = raw_input("ingrese algun texto :")
 print (' '.join(format(ord(x), 'b') for x in st))
 
 #Pd:el nacho se la come xD
+#tenemos que respetar un poco mas los roles para hacer participar a los demas compas
+#si es necesario volver a designar los roles (no se si se puede?) 
+#conrespeto
